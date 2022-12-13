0a10470d46db0dff1450ad29b6ef574d86e8dba9
Luyao <52010097+luyaozhang-1@users.noreply.github.com>
2022-12-13 19:46:40 +0100

    Update SampleWithMistakes.java
    Change a mistake

--- SampleWithMistakes.java
+++ SampleWithMistakes.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int x = 10;
         int y = 5;
-         System.out.printLn("The sum is: " + calculateSum(x, y));
+        System.out.printLn("The sum is: " + calculateSum(x, y));
     }
 
     private int calculateSum (int x, int y) {
-        double sum = x + y;
+        char sum = x + y;
         return sum;
     }
 }
