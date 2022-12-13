dd7342ac58734da8521cc606bcd9773f17c0ca5c
Luyao <52010097+luyaozhang-1@users.noreply.github.com>
2022-12-13 19:19:59 +0100

    Create SampleWithMistakes.java
    Code sample with mistakes.

---
+++ SampleWithMistakes.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+         System.out.printLn("The sum is: " + calculateSum(x, y));
+    }
+
+    private int calculateSum (int x, int y) {
+        double sum = x + y;
+        return sum;
+    }
+}
